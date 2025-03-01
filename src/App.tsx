020751e45634eb603440d7d34bc2c4ed0552f214
Keshav Shah <keshavrshah@gmail.com>
2025-03-01 18:53:52 -0500

    Gemplay setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Button } from "./components/ui/button"
+
+export default function App() {
+  return (
+    <div>
+      <Button>Test</Button>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+
+  )
+}
